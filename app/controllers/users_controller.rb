d1f14c2609ec1d6adcef2caaf3bd12ee6bd122ce
Timi Ajiboye <timi@helloworld.ng>
2016-09-25 15:24:06 +0100

    fixed params syntax in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,13 @@ class UsersController < ApplicationController
 
   def create
     param! :uid, String, required: true
-    @user = User.create(uid: param[:uid])
+    @user = User.create(uid: params[:uid])
     render json: @user, status: 201
   end
 
   def destroy
     param! :id, String, required: true
-    @user = User.find_by_uid(param[:id])
+    @user = User.find_by_uid(params[:id])
     @user.destroy
     head status: 204
   end
