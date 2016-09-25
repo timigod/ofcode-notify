975233f6f445cdae033f19e5d7d046390cc8f182
Timi Ajiboye <timi@helloworld.ng>
2016-09-25 15:14:25 +0100

    same as last

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:7200/','https://app.bitsofco.de'
+    origins 'localhost:7200','https://app.bitsofco.de'
 
     resource '*',
       headers: :any,
